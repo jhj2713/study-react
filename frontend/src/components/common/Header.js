a62c0ab7274893f7e3376a9dddf0078226f6dd63
juhyojeong <juhyojeong58@gmail.com>
2021-10-20 17:30:09 +0900

    Add Header Container

--- frontend/src/components/common/Header.js
+++ frontend/src/components/common/Header.js
@@ -27,8 +27,12 @@ const HeaderBlock = styled.div`
   background: white;
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.08);
 `;
+const UserInfo = styled.div`
+  font-weight: 800;
+  margin-right: 1rem;
+`;
 
-const Header = () => {
+const Header = ({ user }) => {
   return (
     <>
       <HeaderBlock>
@@ -36,9 +40,16 @@ const Header = () => {
           <Link to="/" className="logo">
             REACT
           </Link>
-          <div className="right">
-            <Button to="/login">로그인</Button>
-          </div>
+          {user ? (
+            <div className="right">
+              <UserInfo>{user.username}</UserInfo>
+              <Button>로그아웃</Button>
+            </div>
+          ) : (
+            <div className="right">
+              <Button to="/login">로그인</Button>
+            </div>
+          )}
         </Wrapper>
       </HeaderBlock>
       <Spacer />
